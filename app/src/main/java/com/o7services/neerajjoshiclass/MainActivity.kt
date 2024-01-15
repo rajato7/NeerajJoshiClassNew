adeb1ab7786a625b30dd40299a8837671139499b
Amanpreet Kaur <69182131+amanpreeto7@users.noreply.github.com>
2024-01-15 17:44:23 +0530

    Constraint Activity Added

--- app/src/main/java/com/o7services/neerajjoshiclass/MainActivity.kt
+++ app/src/main/java/com/o7services/neerajjoshiclass/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnConstraint: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnConstraint = findViewById(R.id.btnConstraint)
+
+        btnConstraint.setOnClickListener{
+            var intent = Intent(this, ConstraintActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
