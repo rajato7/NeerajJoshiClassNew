19d5c45900b7fd0e9c439f1e090ff1c3c2fe17e2
Rajat <intern163@o7tec.com>
2024-01-25 11:01:19 +0530

    check description
    Enabled View binding

--- app/src/main/java/com/o7services/neerajjoshiclass/GetValueActivity.kt
+++ app/src/main/java/com/o7services/neerajjoshiclass/GetValueActivity.kt
@@ -6,35 +6,38 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.o7services.neerajjoshiclass.databinding.ActivityGetValueBinding
 
 class GetValueActivity : AppCompatActivity() {
-    lateinit var etName : EditText // declare the EditText
-    lateinit var etRollNo: EditText
-    lateinit var btnSave: Button
-    lateinit var tvName: TextView
-    lateinit var tvRollNo: TextView
+    lateinit var binding: ActivityGetValueBinding // declare binding
+//    lateinit var etName : EditText // declare the EditText
+//    lateinit var etRollNo: EditText
+//    lateinit var btnSave: Button
+//    lateinit var tvName: TextView
+//    lateinit var tvRollNo: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_value)
-        etName = findViewById(R.id.etName) // intialize
-        etRollNo = findViewById(R.id.etRollNo)
-        btnSave= findViewById(R.id.btnSave)
-        tvName= findViewById(R.id.tvName)
-        tvRollNo= findViewById(R.id.tvRollNo)
+        binding = ActivityGetValueBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        etName = findViewById(R.id.etName) // intialize
+//        etRollNo = findViewById(R.id.etRollNo)
+//        btnSave= findViewById(R.id.btnSave)
+//        tvName= findViewById(R.id.tvName)
+//        tvRollNo= findViewById(R.id.tvRollNo)
 
-        btnSave.setOnClickListener{
-            if(etName.text.toString().isEmpty()){
-                etName.error = "Enter your name"
-            }else if(etRollNo.text.toString().isEmpty()){
-                etRollNo.error = "Enter Your Roll No."
+        binding.btnSave.setOnClickListener{
+            if(binding.etName.text.toString().isEmpty()){
+                binding.etName.error = "Enter your name"
+            }else if(binding.etRollNo.text.toString().isEmpty()){
+                binding.etRollNo.error = "Enter Your Roll No."
             }else{
-                println("My Name: ${etName.text.toString()},My Roll No: ${etRollNo.text.toString()} ")
+                println("My Name: ${binding.etName.text.toString()},My Roll No: ${binding.etRollNo.text.toString()} ")
                 var intent = Intent(this,SecondActivity::class.java)
-                intent.putExtra("name",etName.text.toString())
-                intent.putExtra("rollNo",etRollNo.text.toString())
+                intent.putExtra("name",binding.etName.text.toString())
+                intent.putExtra("rollNo",binding.etRollNo.text.toString())
                 startActivity(intent)
-                tvName.text = etName.text.toString()
-                tvRollNo.text = etRollNo.text.toString()
+                binding.tvName.text = binding.etName.text.toString()
+                binding.tvRollNo.text = binding.etRollNo.text.toString()
             }
         }
 
